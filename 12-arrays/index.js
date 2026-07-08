d6f2e7a783c2672d771621999139a99bfb2881ea
prathmesh gawand <prathameshgawand01@gmail.com>
2026-07-08 13:44:54 +0530

    feat: learn JavaScript arrays

---
+++ 12-arrays/index.js
@@ -0,0 +1,15 @@
+let marks_class_12 =[91, 82, 63, 74 ,null , false , "Not Present"]
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6])
+console.log(marks_class_12[7])
+console.log("The length of marks_class_12 is" , marks_class_12.length)
+marks_class_12[7] = 94   
+marks_class_12[0] = 96   
+console.log(marks_class_12)
+console.log(typeof marks_class_12)
