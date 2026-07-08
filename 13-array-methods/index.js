5606161a05bc2ec496a28ee787debe443caa3d1b
prathmesh gawand <prathameshgawand01@gmail.com>
2026-07-08 14:06:43 +0530

    feat: learn JavaScript array methods

---
+++ 13-array-methods/index.js
@@ -0,0 +1,16 @@
+let num = [1, 2, 3, 34, 35]
+let b = num.toString()
+console.log(b , typeof b)
+let c = num.join("_")
+console.log(c , typeof c)
+// let r = num.pop()
+// console.log(num, r)
+
+// let r = num.push(56)
+// console.log(num, r)
+
+// let r = num.shift()
+// console.log(num, r)
+
+let r = num.unshift(11)
+console.log(num, r)
